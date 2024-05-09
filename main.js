2cb5fa7908f8444d7ab409b3aface6caec65a59f
Jenny <jennylee1989@gmail.com>
2024-05-09 10:44:50 -0700

    style and headline

--- main.js
+++ main.js
@@ -10,12 +10,7 @@ var video = document.getElementById("myVideo");
     });
 
 
-// images growing
-document.querySelectorAll('.js-t-line').forEach((element, index) => {
-    setTimeout(() => {
-        element.style.transform = `translate(0px, ${index * 30}px)`;
-    }, index * 1000);
-    });
+
 
 // navbar changing color
 window.onscroll = function() {
@@ -68,3 +63,42 @@ function addAnimation() {
     });
   });
 }
+
+
+// tagline words
+const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const enhance = id => {
+  const element = document.getElementById(id),
+        text = element.innerText.split("");
+  
+  element.innerText = "";
+  
+  text.forEach((value, index) => {
+    const outer = document.createElement("span");
+    
+    outer.className = "outer";
+    
+    const inner = document.createElement("span");
+    
+    inner.className = "inner";
+    
+    inner.style.animationDelay = `${rand(-5000, 0)}ms`;
+    
+    const letter = document.createElement("span");
+    
+    letter.className = "letter";
+    
+    letter.innerText = value;
+    
+    letter.style.animationDelay = `${index * 1000 }ms`;
+    
+    inner.appendChild(letter);    
+    
+    outer.appendChild(inner);    
+    
+    element.appendChild(outer);
+  });
+}
+
+enhance("channel-link");
